3be486433f10ce69ccda2495dbafa26074b65d7e
chentao <chentao@chinamcloud.com>
2021-01-07 15:26:57 +0800

    学习一哈

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+from jd_mask_spider_requests import Jd_Mask_Spider
+
+if __name__ == '__main__':
+    a = """
+ *                            _ooOoo_
+ *                           o8888888o
+ *                           88" . "88
+ *                           (| -_- |)
+ *                            O\ = /O
+ *                        ____/`---'\____
+ *                      .   ' \\| |// `.
+ *                       / \\||| : |||// \
+ *                     / _||||| -:- |||||- \
+ *                       | | \\\ - /// | |
+ *                     | \_| ''\---/'' | |
+ *                      \ .-\__ `-` ___/-. /
+ *                   ___`. .' /--.--\ `. . __
+ *                ."" '< `.___\_<|>_/___.' >'"".
+ *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ *         ======`-.____`-.___\_____/___.-`____.-'======
+ *                            `=---='
+ *
+ *         .............................................
+ *                  佛祖镇楼                  BUG辟易
+ *          佛曰:
+ *                  写字楼里写字间，写字间里程序员；
+ *                  程序人员写程序，又拿程序换酒钱。
+ *                  酒醒只在网上坐，酒醉还来网下眠；
+ *                  酒醉酒醒日复日，网上网下年复年。
+ *                  但愿老死电脑间，不愿鞠躬老板前；
+ *                  奔驰宝马贵者趣，公交自行程序员。
+ *                  别人笑我忒疯癫，我笑自己命太贱；
+ *                  不见满街漂亮妹，哪个归得程序员？
+ 
+ 
+ 
+ *                  1.预约商品
+ *                  2.秒杀抢购商品 
+    """
+    start_tool = Jd_Mask_Spider()
+    print(a)
+    choice_function = input('选择功能:')
+    if choice_function == '1':
+        start_tool.login()
+        start_tool.make_reserve()
+    elif choice_function == '2':
+        start_tool.request_seckill_url()
+        start_tool.request_seckill_checkout_page()
+        start_tool.submit_seckill_order()
+    else:
+        print('没有此功能')
+        sys.exit(1)
